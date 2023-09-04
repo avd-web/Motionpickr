b378b24c9836b3574593744bb5b2f4cdff0ad630
aegroot <alex-groot@live.nl>
2023-09-04 11:15:02 +0200

    init

---
+++ src/main/java/com/ateam/motionpickr/MotionpickrApplication.java
@@ -0,0 +1,13 @@
+package com.ateam.motionpickr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotionpickrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotionpickrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ateam/motionpickr/MotionpickrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ateam.motionpickr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotionpickrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
