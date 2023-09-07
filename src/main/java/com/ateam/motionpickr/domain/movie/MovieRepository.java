123f454c80f2155d6c9df198c951a4b786511841
Alexandernevsk <Alexandertorreman@gmail.com>
2023-09-07 12:35:11 +0200

    Added the movie-endpoints

--- src/main/java/com/ateam/motionpickr/domain/movie/MovieRepository.java
+++ src/main/java/com/ateam/motionpickr/domain/movie/MovieRepository.java
@@ -3,6 +3,9 @@ package com.ateam.motionpickr.domain.movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    Optional<Movie> findByTitle(String title);
 }
