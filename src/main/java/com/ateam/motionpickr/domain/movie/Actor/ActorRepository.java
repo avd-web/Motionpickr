50b9ccd85f2d6c77a74e01d4a81639f36029104a
aegroot <alex-groot@live.nl>
2023-09-07 12:28:32 +0200

    update

---
+++ src/main/java/com/ateam/motionpickr/domain/movie/Actor/ActorRepository.java
@@ -0,0 +1,9 @@
+package com.ateam.motionpickr.domain.movie.Actor;
+
+import com.ateam.motionpickr.domain.actor.Actor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ActorRepository extends JpaRepository<Actor,Long> {
+}
