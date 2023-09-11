16615648e4d197d9143d489c4d09142749f265b1
aegroot <alex-groot@live.nl>
2023-09-11 14:35:29 +0200

    added link component for navbar

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import "./navbar.css";
-import NavLink from '../navbar/navlink/navLink.jsx'
-
+import NavLink from "../navbar/navlink/navLink.jsx";
+import { LinkData } from "./linkData";
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
-    <NavLink link={"/movie"} name={"movies"}/>
-    
+    <div className="navbar">
+      {console.log(LinkData)}
+      {LinkData.map(item>= <NavLink name={item} link={item} />)}
     </div>
-  )
+  );
 }
