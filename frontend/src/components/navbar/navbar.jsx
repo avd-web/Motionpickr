ed3f486e8f664c43d67fc64221d2ce47139c3296
aegroot <alex-groot@live.nl>
2023-09-13 11:22:40 +0200

    updated navbar component to be more dynamic

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import "./navbar.css";
 import NavLink from "../navbar/navlink/navLink.jsx";
-import { LinkData } from "./linkData";
 
-export default function Navbar() {
+export default function Navbar({links}) {
   return (
     <div className="navbar">
-      <h2>navbar</h2>
-      <div className="navbar-list-container">
-        
+      <h2>navbar</h2>       
         <div className="list-container">
-          {LinkData.map((item) => (
+          {links.map((item) => (
             <NavLink name={item.name} link={item.link} />
           ))}
-        </div>
       </div>
     </div>
   );
