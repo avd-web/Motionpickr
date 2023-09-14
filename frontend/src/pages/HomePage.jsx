73f6bf804811e1f7f6f145ff4ab4de160ee19342
Alexandernevsk <Alexandertorreman@gmail.com>
2023-09-14 15:22:50 +0200

    Made some search functions

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GenreItem from "../components/homepagecomponents/GenreDropDown";
 import Hero from "../components/hero/Hero";
 import MovieList from "../components/moviecomponents/MovieList";
+import axios from "axios";
 
 export default function HomePage() {
+  const [movies, setMovies] = useState([]);
+  const fetchMovies = async () => {
+    const movieFetch = await axios.get(
+      `http://localhost:8080/api/v1/movie?page=0&size=10`
+    );
+    setMovies(movieFetch.data.content);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <>
       <Hero></Hero>
       <GenreItem />
-      {/* <MovieList /> */}
+      {movies.map((movie) => (
+        <MovieList movie={movie} key={movie.id} />
+      ))}
     </>
   );
 }
