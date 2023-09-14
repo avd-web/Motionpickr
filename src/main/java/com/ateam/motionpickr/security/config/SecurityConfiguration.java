0ba0390c4120728091e0e230d71f69a00a193e57
aegroot <alex-groot@live.nl>
2023-09-14 13:31:04 +0200

    added genre path

--- src/main/java/com/ateam/motionpickr/security/config/SecurityConfiguration.java
+++ src/main/java/com/ateam/motionpickr/security/config/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration {
                 .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**")
+                        .requestMatchers("/api/v1/auth/**","api/v1/movie/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
