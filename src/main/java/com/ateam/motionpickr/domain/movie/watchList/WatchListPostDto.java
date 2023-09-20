6a2bee89c7abca7c98fdb1c1a532b85e61ea1ae6
aegroot <alex-groot@live.nl>
2023-09-20 13:45:04 +0200

    made WatchList endpoints

---
+++ src/main/java/com/ateam/motionpickr/domain/movie/watchList/WatchListPostDto.java
@@ -0,0 +1,15 @@
+package com.ateam.motionpickr.domain.movie.watchList;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class WatchListPostDto {
+    private long movieId;
+}
