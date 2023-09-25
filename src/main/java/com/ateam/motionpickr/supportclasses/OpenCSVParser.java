fab62b187aa31061254556896d0083d81cc0c056
Alexandernevsk <Alexandertorreman@gmail.com>
2023-09-25 15:58:40 +0200

    Test Actors

--- src/main/java/com/ateam/motionpickr/supportclasses/OpenCSVParser.java
+++ src/main/java/com/ateam/motionpickr/supportclasses/OpenCSVParser.java
@@ -1,8 +1,8 @@
 package com.ateam.motionpickr.supportclasses;
 
+import com.ateam.motionpickr.domain.genre.Genre;
 import com.ateam.motionpickr.domain.genre.GenreRepository;
 import com.ateam.motionpickr.domain.movie.Movie;
-import com.ateam.motionpickr.domain.genre.Genre;
 import com.ateam.motionpickr.domain.movie.MovieRepository;
 import com.opencsv.CSVReader;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +12,10 @@ import org.springframework.stereotype.Component;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class OpenCSVParser implements CommandLineRunner {
@@ -25,7 +28,34 @@ public class OpenCSVParser implements CommandLineRunner {
 
     @Value("${motionpickr.dir}")
     private String filename;
+
+    @Value("${motionpickrCast.dir}")
+    private String filenameCast;
+
+    private final Set<String> knownActors = new HashSet<>();
     private final Set<String> knownGenres = new HashSet<>();
+
+
+    public void getActor(String[] line) {
+        String actor = line[0];
+
+        for (String values : actor.split("\\[")) {
+            var fields = values.split("}");
+            for (String objects : fields) {
+                String[] actorFields = objects.split(",");
+                for (String string : actorFields) {
+                    if (string.contains("name")) {
+                        String name = string.replaceAll("'name': ", "");
+                        knownActors.add(name.substring(2, name.length() - 1));
+                        System.out.println(name.substring(2, name.length() - 1));
+                    }
+                }
+            }
+        }
+    }
+
+
+
 
     private Movie createMovieFromArray(String[] line) {
         String genres = line[3];
