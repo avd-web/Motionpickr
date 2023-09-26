530fdfaab372930a774d3d8298c77f6722bbc59d
aegroot <alex-groot@live.nl>
2023-09-26 12:20:07 +0200

    reviews and watchlist working properly

--- frontend/src/components/systemRecommended/systemRecommendedList.jsx
+++ frontend/src/components/systemRecommended/systemRecommendedList.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function systemRecommendedList() {
+    const [recommended,setRecommended]=useState([]);
+
+
   return (
     <div>systemRecommendedList</div>
   )
