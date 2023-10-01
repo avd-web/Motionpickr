100ecf92af040ba79616635b5af16246aa268821
avd <avd-web@outlook.com>
2023-10-01 15:19:31 +0200

    save changes, while working on login structure FIX

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import "../styles/registerSheet.css";
 import axios from "axios";
 import { AuthHeader } from "../auth/authorization";
+import { useEffect } from "react";
+import Register from "./Register";
 
 export default function Login() {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(true);
+  const [auth, setAuth] = useState(null);
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -23,58 +26,76 @@ export default function Login() {
     sessionStorage.clear();
     sessionStorage.setItem("access_token", authResp.data.access_token);
     sessionStorage.setItem("refres_token", authResp.data.refresh_token);
-    sessionStorage.setItem("user_email",email);
+    sessionStorage.setItem("user_email", email);
 
-    console.log(AuthHeader());
-    let demo = await axios.get(
-      "http://localhost:8080/api/v1/demo-controller",
-      AuthHeader()
-    );
-    console.log(demo);
+    setShow(sessionStorage.getItem("key"));
+
+    // console.log(AuthHeader());
+    // let demo = await axios.get(
+    //   "http://localhost:8080/api/v1/demo-controller",
+    //   AuthHeader()
+    // );
+    // console.log(demo);
+
     emptyForm();
   };
   const emptyForm = () => {
     setEmail("");
     setPassword("");
-    setShow(false);
+    // setShow(false);
   };
 
-  return (
-    <div className="register-container">
-      <h1 className="register-header"> Sign in </h1>
-      <div className="label-container">
-        <form className="register-labels" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="email" id="email-label">
-              Enter e-mail:
-            </label>
-            <input
-              type="text"
-              id="email"
-              required
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-          </div>
-          <div>
-            <label htmlFor="password" id="password-label">
-              Enter password:
-            </label>
-            <input
-              type={show ? "text" : "password"}
-              id="password"
-              required
-              value={password}
-              onChange={(ev) => {
-                setPassword(ev.target.value);
-              }}
-            />
-            <button type="submit">login</button>
-          </div>
-        </form>
+  useEffect(() => {
+    console.log("useeffect activated");
+    if (sessionStorage.getItem("key")) {
+      setShow(false);
+      console.log("key set:");
+      console.log(sessionStorage.getItem("key"));
+      console.table(sessionStorage.getItem("key"));
+    }
+  }, []);
+
+  if (show) {
+    return (
+      <div className="register-container">
+        <h1 className="register-header"> Sign in </h1>
+        <div className="label-container">
+          <form className="register-labels" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email" id="email-label">
+                Enter e-mail:
+              </label>
+              <input
+                type="text"
+                id="email"
+                required
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" id="password-label">
+                Enter password:
+              </label>
+              <input
+                type="password"
+                id="password"
+                required
+                value={password}
+                onChange={(ev) => {
+                  setPassword(ev.target.value);
+                }}
+              />
+              <button type="submit">login</button>
+            </div>
+          </form>
+        </div>
+        <div>
+          <Register />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
