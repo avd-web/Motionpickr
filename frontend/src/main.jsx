a02260dee531ac63595d6fb04d79f2588ff34c9d
Arjan <avd-web@outlook.com>
2023-10-02 21:04:01 +0200

    add CSS part 1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,8 +9,9 @@ import ViewMovie from "./pages/viewMovie";
 import Recommended from "./pages/userRecommended";
 import GenreInfo from "./pages/GenreInfo";
 import Genre from "./pages/Genre";
-import "./styles/MAINstyles.css";
-import "./styles/normalize.css";
+// import "./styles/MAINstyles.css";
+import "./styles/MainStyles.scss";
+// import "./styles/normalize.css";
 import UserPage from "./pages/UserPage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
