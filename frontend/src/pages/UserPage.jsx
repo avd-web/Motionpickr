ccb54384965c64cc86f1b657f47d26f243407e3e
avd <avd-web@outlook.com>
2023-10-04 12:00:28 +0200

    implementing basic css part 10+ also added Logout button

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./Login";
 import { AuthHeader } from "../auth/authorization";
 import WatchList from "./watchList";
+import axios from "axios";
 
 export default function UserPage() {
+  const [user, setUser] = useState();
+
+  async function userInfo() {
+    if (sessionStorage.getItem("access_token")) {
+      const user = await axios.get(
+        `http://localhost:8080/api/v1/user`,
+        AuthHeader()
+      );
+
+      // const user = sessionStorage.getItem("access_token")
+      console.log(user.data);
+      setUser(user.data.user);
+    }
+  }
+
+  useEffect(() => {
+    userInfo();
+  }, []);
+
   if (AuthHeader()) {
     return (
       <>
